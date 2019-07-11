43f9354e373db6ba30017aeedad2a72e9a48735f
hj24 <huangjian24@foxmail.com>
2019-07-11 22:19:18 +0800

    📌 更新v1.0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/hj24/fuck-spoiler'
 EMAIL = 'mambahj24@gmail.com'
 AUTHOR = 'hj24'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
